d15f8360b254e8130bdd5b378f10ff3d53fa4f72
Bo <bzh@live.com>
2024-02-04 00:44:08 +0800

    Add lsky backup script

---
+++ sh/lsky-backup.sh
@@ -0,0 +1,3 @@
+rclone sync /opt/lsky/web/storage dropbox:lsky/storage
+rclone sync /opt/lsky/web/public/thumbnails dropbox:lsky/public/thumbnails
+rclone sync /opt/lsky/web/database dropbox:lsky/database
